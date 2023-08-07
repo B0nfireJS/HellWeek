4a52a288b4de58b8f24d776ef78b1de38beeabb9
B0nfireJS <karate123123123@gmail.com>
2023-08-08 01:04:14 +0400

    недоделанный десктоп, остановился на добавлениии карточек с сетями, где выдаёт ошибку из-за неправильного пути в пикчах

---
+++ src/js/modules/Slider.js
@@ -0,0 +1,21 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  loop: true,
+  slidesPerView: "auto",
+  spaceBetween: -20,
+  centeredSlides: true,
+  speed: 600,
+  // centeredSlidesBounds: true
+});
--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -1,2 +1,3 @@
 console.log('Webpack + Pug starter')
+import Swiper from "../modules/Slider";
 import "../modules/test";
