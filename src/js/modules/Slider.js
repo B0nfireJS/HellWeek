446d0c32ccba82117b4bf285b3b9ceb5ee0a6f3e
B0nfireJS <karate123123123@gmail.com>
2023-08-28 19:20:25 +0400

    Первый день

--- src/js/modules/Slider.js
+++ src/js/modules/Slider.js
@@ -4,28 +4,23 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
-// init Swiper:
 const swiper = new Swiper('.swiper', {
-  // configure Swiper to use modules
   modules: [Navigation, Pagination],
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
   loop: true,
   slidesPerView: "auto",
   spaceBetween: -20,
   centeredSlides: true,
   speed: 600,
-  // centeredSlidesBounds: true
-  breakpoints: {
-    320:{
-      slidesPerView: "1.2",
-      spaceBetween: -5,
-    },
-    992:{
-      slidesPerView: "auto",
-      spaceBetween: -20,
-    }
-  }
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -1,4 +1,4 @@
 console.log('Webpack + Pug starter')
 import Swiper from "../modules/Slider";
 import "../modules/test";
-import "../modules/Burger";
+import "../modules/Tabs";
